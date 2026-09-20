7642c32fe6e4866d158e933b26d3c7700cb46a09
oclaudiodev <7claudiosouza@gmail.com>
2026-09-20 13:21:18 -0300

    implementa components header e footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './shared/menu/menu';
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Menu,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('carSales');
+  titulo = 'carSales';
+  nome = 'José'
 }
+
---
+++ src/app/shared/menu/menu.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-menu',
+  styleUrl: './menu.css',
+  templateUrl: './menu.html',
+})
+
+export class Menu {
+  
+}
